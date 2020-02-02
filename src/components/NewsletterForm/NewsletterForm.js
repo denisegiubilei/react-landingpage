4728c560e97c3d5f85cafae97e46cf90a58745fb
denisegiubilei <giubileidenise@gmail.com>
2020-02-02 00:30:39 -0300

    fix: reponsive of newsletter component

--- src/components/NewsletterForm/NewsletterForm.js
+++ src/components/NewsletterForm/NewsletterForm.js
@@ -26,14 +26,8 @@ const NewsletterForm = () => {
       <p>Cadastre-se e fique por dentro das novidades ;)</p>
       <form id="newsletter-form" onSubmit={submitNewsletter}>
         <div className="fields">
-          <div className="field">
-            <label htmlFor="name">Nome:</label>
-            <input type="text" id="name" name="name" required min-length="2" className="input"/>
-          </div>
-          <div className="field">
-            <label htmlFor="name">E-mail:</label>
-            <input type="email" id="email" name="email" required className="input"/>
-          </div>
+          <input type="text" id="name" name="name" placeholder="Nome" required min-length="2" className="input"/>
+          <input type="email" id="email" name="email" placeholder="Endereço de e-mail" required className="input"/>
         </div>
         <input type="submit" className="button regular" value={submitButtonText}/>
       </form>
