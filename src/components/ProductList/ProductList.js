4fd06a28ab4c5ea91232474bda68b5e8ca3d0003
Denise Giubilei <giubileidenise@gmail.com>
2020-12-15 21:34:26 -0300

    disable quickview on mobile

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import { useIsMobile } from '../Utils/windowSize';
 import { ProductCard } from '../ProductCard';
 
 import './ProductList.css';
 
-const ProductList = ({ products = [] }) => (
-  <div className="product-list">
-    {products.map((product) => (
-      <ProductCard product={product} key={product.id} enableQuickView />
-    ))}
-  </div>
-);
+const ProductList = ({ products = [] }) => {
+  const isMobile = useIsMobile();
+  return (
+    <div className="product-list">
+      {products.map((product) => (
+        <ProductCard product={product} key={product.id} enableQuickView={!isMobile} />
+      ))}
+    </div>
+  );
+};
 
 export default ProductList;
